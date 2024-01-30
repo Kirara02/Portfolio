97bf4c13c57faf0d9b1bf22c5646186a33f45217
Kirara02 <fathul.sirnaraja92@gmail.com>
2024-01-30 15:59:27 +0700

    add assets

---
+++ lib/config/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xffF2F5F9);
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff000000);
+  static const Color black2 = Color(0xff1A1003);
+  static const Color dark = Color(0xff0B0909);
+  static const Color grey = Color(0xffE1E8EF);
+  static const Color orange = Color(0xffFF9C1A);
+  static const Color red = Color(0xffFF9C1A);
+}
